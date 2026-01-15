20480cc1b1ed882c88ed49125c9de43d391e1c64
Tatielle Dias ( Taty) <tatielle2019@gmail.com>
2026-01-15 16:23:57 -0300

    Refactor API tests for clarity and structure

--- app.test.js
+++ app.test.js
@@ -1,15 +1,8 @@
 const request = require('supertest');
 const app = require('./app');
 
-describe('Testes da API', () => {
-  it('Deve criar uma tarefa', async () => {
-    const res = await request(app)
-      .post('/tarefas')
-      .send({ titulo: 'Estudar' });
-    expect(res.statusCode).toEqual(201);
-  });
-
-  it('Deve listar tarefas', async () => {
+describe('API Tests', () => {
+  it('should list tasks', async () => {
     const res = await request(app).get('/tarefas');
     expect(res.statusCode).toEqual(200);
   });
