37b3e56bc1c01888527995ff0b7e3bf4e4bc4fdb
Tatielle Dias ( Taty) <tatielle2019@gmail.com>
2026-01-15 15:58:46 -0300

    Add API tests for task creation and listing

--- app.test.js
+++ app.test.js
@@ -1,10 +1,20 @@
 const request = require('supertest');
 const app = require('./app');
 
-test('Deve criar uma nova tarefa com sucesso', async () => {
+describe('Testes da API de Tarefas', () => {
+  it('Deve criar uma nova tarefa com sucesso', async () => {
     const res = await request(app)
-        .post('/tarefas')
-        .send({ titulo: 'Estudar para a FECAF' });
+      .post('/tarefas')
+      .send({
+        titulo: 'Estudar para a FECAF'
+      });
     expect(res.statusCode).toEqual(201);
     expect(res.body.titulo).toBe('Estudar para a FECAF');
+  });
+
+  it('Deve listar as tarefas com sucesso', async () => {
+    const res = await request(app).get('/tarefas');
+    expect(res.statusCode).toEqual(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
 });
